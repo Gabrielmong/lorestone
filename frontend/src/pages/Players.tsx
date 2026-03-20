0674a327c39a708a49ff33a6580da14c72547a53
Gabrielmong <gabriel.monge.lizano@gmail.com>
2026-03-19 23:36:04 -0600

    feat: more integration changes

--- frontend/src/pages/Players.tsx
+++ frontend/src/pages/Players.tsx
@@ -181,7 +181,7 @@ function formToInput(v: PlayerFormValues, importType: ImportType) {
   }
 }
 
-const API_BASE = 'http://localhost:4000'
+const API_BASE = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:4000'
 
 export default function Players() {
   const { campaignId } = useCampaign()
